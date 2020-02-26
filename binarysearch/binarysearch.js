4f448b1fa4604c806af33b7d2f9daa5b013d8f17
DannyBrito <britodanny1997@gmail.com>
2020-02-25 21:44:07 -0500

    binary search

---
+++ binarysearch/binarysearch.js
@@ -0,0 +1,13 @@
+export function search(arr,val){
+    let left = 0
+    let right = arr.length - 1 
+    while (left < right){
+        let middle = Math.ceil((left+right)/2)
+        if(arr[middle] === value) return middle
+        else if(arr[middle] > value){
+            right = middle 
+        }
+        else left = middle 
+    }
+    return -1
+}
