8f59517e38f5cc55cc7a34cbbcf3db134e6c9d3a
DannyBrito <britodanny1997@gmail.com>
2020-03-18 21:12:42 -0400

    pyramid

---
+++ pyramid/index.js
@@ -0,0 +1,11 @@
+function pyramid(n,row = 0,level = ''){
+    let len = n * 2 - 1
+    let mid = Math.floor(len/2)
+    if(n === row)return
+    if(len === level.length){
+        console.log(level)
+        return pyramid(n,row+1)
+    }
+    const add = (level.length >= mid - row && level.length <= mid + row)? '#':' '
+    pyramid(n,row,level + add)
+}
