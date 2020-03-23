cd7ea175d3ed7d700f13b32adf074d37aa789bc7
DannyBrito <britodanny1997@gmail.com>
2020-03-22 21:01:30 -0400

    cont dup

--- exercises/containsdupicate.js
+++ exercises/containsdupicate.js
@@ -1,4 +1,5 @@
 var containsDuplicate = function(nums) {
+    // use a object to keep track of the frequency of the characters
     let seen = {}
     for(const e of nums){
         if(seen[e]) return true
