6061b938c50be80b4ab8a67b1d9b686b8d496634
DannyBrito <britodanny1997@gmail.com>
2020-03-23 21:26:55 -0400

    cdps

--- exercises/containsdupicate.js
+++ exercises/containsdupicate.js
@@ -2,6 +2,7 @@ var containsDuplicate = function(nums) {
     // use a object to keep track of the frequency of the characters
     let seen = {}
     for(const e of nums){
+        // if existeng return true as there is duplicates
         if(seen[e]) return true
         seen[e] = true
     }
