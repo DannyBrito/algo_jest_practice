e006952b49419dde8aa0d6e543d3d163c9773a52
DannyBrito <britodanny1997@gmail.com>
2020-04-06 12:51:41 -0400

    gts

--- binarysearch/binarysearch.js
+++ binarysearch/binarysearch.js
@@ -1,6 +1,7 @@
 export function search(arr,val){
     let left = 0
     // divide and conquer
+    // we divide a large set of data on d&c 
     let right = arr.length - 1 
     while (left < right){
         let middle = Math.ceil((left+right)/2)
