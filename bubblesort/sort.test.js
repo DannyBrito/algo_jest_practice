f5a99babf164616e9ace9cccf6c2795c508aa7d5
DannyBrito <britodanny1997@gmail.com>
2020-04-16 13:38:56 -0400

    test for bubblesort;

--- bubblesort/sort.test.js
+++ bubblesort/sort.test.js
@@ -4,4 +4,7 @@ describe('Bubble Sort',()=>{
     test('testing [3,5,6,1,8] to be [1,3,5,6,8]',()=>{
        expect(bubbleSort([3,5,6,1,8])).toEqual([1,3,5,6,8]) 
     })
+    test('testing [1,0,-4,1,2,1,8] to be [-4,0,1,1,1,2,8]',()=>{
+       expect(bubbleSort([1,0,-4,1,2,1,8])).toEqual([-4,0,1,1,1,2,8]) 
+    })
 })
