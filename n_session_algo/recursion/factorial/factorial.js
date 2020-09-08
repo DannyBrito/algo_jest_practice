6cd2e994a787066e0ef7a79ab22c36cca343d21e
DannyBrito <britodanny1997@gmail.com>
2020-09-08 19:40:38 -0400

    fix base case for factorial

--- n_session_algo/recursion/factorial/factorial.js
+++ n_session_algo/recursion/factorial/factorial.js
@@ -1,5 +1,5 @@
 // Recursion has two important sections, a base case as ending condition for the function and a recursive call
 export const factorial = num =>{
-    if(num === 1) return 1
+    if(num <= 1) return 1
     return num * factorial(num - 1)
 }
