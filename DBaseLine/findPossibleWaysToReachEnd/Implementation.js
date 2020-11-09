9ab47423fd3f5f90c7cc39b852fbdab55e72a6ba
DannyBrito <britodanny1997@gmail.com>
2020-11-09 11:46:21 -0500

    problem description addded

---
+++ DBaseLine/findPossibleWaysToReachEnd/Implementation.js
@@ -0,0 +1,14 @@
+// need to find number of possibles ways to reach to bottom-right corner
+// of matrix starting from top-left corner. You can only move to right 
+// or bottom. The input matrix given will represent the map/path to travel filled with either a 1 or 0. 1 meaning you can access the cell and 0 the cell is block and you need to find other path.
+
+/*
+[
+    [1,0,1],
+    [1,1,1],
+    [0,0,1],
+]
+return should be 1 as there is only one avalible path to go from start 
+to end
+*/
+
