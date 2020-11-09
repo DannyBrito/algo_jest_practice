ce20decfe5c4238e52926e55d56a5599da521b22
DannyBrito <britodanny1997@gmail.com>
2020-11-09 12:11:55 -0500

    problem implementation added where paths could be block, the first left-top cell from matrix must always be valid

--- DBaseLine/findPossibleWaysToReachEnd/Implementation.js
+++ DBaseLine/findPossibleWaysToReachEnd/Implementation.js
@@ -10,5 +10,32 @@
 ]
 return should be 1 as there is only one avalible path to go from start 
 to end
+
+ * @param {2D Array[Number]} matrix
+ * @return {number}
+ * 
 */
 
+export const findPossibleWays = matrix =>{
+    let rows = matrix.length
+    let columns = matrix[0].length
+    let dp = [...new Array(rows)].map(x => new Array(columns).fill(0))
+
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < columns; j++){
+            if(matrix[i][j] !== 0){
+                if(i > 0 && j > 0){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                }
+                else if(i === 0 && j === 0) dp[i][j] = 1
+                else{
+                    dp[i][j] = dp[validateIndex(i-1)][j] + dp[i][validateIndex(j-1)]
+                }
+            }
+        }
+    }
+
+    return dp[rows-1][columns-1]
+}
+
+const validateIndex = i => i < 0 ? 0 : i
